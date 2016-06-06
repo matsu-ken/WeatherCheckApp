66564419fd8364edf431d267a33e0c0b270d4972
matsuoka <matsuoka@PDV9999002.iFORZA.local>
2016-06-06 17:48:49 +0900

    Weather Hacksから天気情報のJSONデータ取得

--- WeatherCheckApp/Form1.cs
+++ WeatherCheckApp/Form1.cs
@@ -7,11 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Codeplex.Data;
+using System.Net;
+
 
 namespace WeatherCheckApp {
   public partial class Form1 : Form {
     public Form1() {
       InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e) {
+      Text = "WeatherCheckApp";
+
+      int id = 400010;
+
+      var url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + id;
+      var req = WebRequest.Create(url);
+
+      using (var res = req.GetResponse())
+      using (var s = res.GetResponseStream()) {
+        dynamic json = DynamicJson.Parse(s);
+      }
+
     }
   }
 }
