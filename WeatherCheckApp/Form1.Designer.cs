4ad211b619abb04e1f8ccf526c9149de6cd4833f
matsuoka <matsuoka@PDV9999002.iFORZA.local>
2016-06-08 18:41:28 +0900

    3日間(今日、明日、明後日)の天気、天気概況の表示

--- WeatherCheckApp/Form1.Designer.cs
+++ WeatherCheckApp/Form1.Designer.cs
@@ -24,34 +24,71 @@
     /// </summary>
     private void InitializeComponent() {
       this.listBox1 = new System.Windows.Forms.ListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
       // listBox1
       // 
       this.listBox1.FormattingEnabled = true;
       this.listBox1.ItemHeight = 12;
-      this.listBox1.Location = new System.Drawing.Point(13, 13);
+      this.listBox1.Location = new System.Drawing.Point(13, 14);
       this.listBox1.Name = "listBox1";
       this.listBox1.Size = new System.Drawing.Size(157, 352);
       this.listBox1.TabIndex = 0;
       this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(200, 20);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 12);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "label1";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(350, 20);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(35, 12);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "label2";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(500, 20);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(35, 12);
+      this.label3.TabIndex = 3;
+      this.label3.Text = "label3";
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(674, 372);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
       this.Controls.Add(this.listBox1);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
   }
 }
 
