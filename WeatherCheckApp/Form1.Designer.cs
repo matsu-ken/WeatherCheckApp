a7fda2ca08ba4a71094411ba21066ceace3a747d
matsuoka <matsuoka@PDV9999002.iFORZA.local>
2016-06-13 17:43:11 +0900

    画像、気温、曜日、thisの追加、var、和暦の変更

--- WeatherCheckApp/Form1.Designer.cs
+++ WeatherCheckApp/Form1.Designer.cs
@@ -25,8 +25,19 @@
     private void InitializeComponent() {
       this.listBox1 = new System.Windows.Forms.ListBox();
       this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this.label3 = new System.Windows.Forms.Label();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
+      this.pictureBox3 = new System.Windows.Forms.PictureBox();
+      this.Temperature = new System.Windows.Forms.Label();
+      this.Date = new System.Windows.Forms.Label();
+      this.Situation = new System.Windows.Forms.Label();
+      this.Date1 = new System.Windows.Forms.Label();
+      this.Date2 = new System.Windows.Forms.Label();
+      this.Temperature1 = new System.Windows.Forms.Label();
+      this.Temperature2 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
       this.SuspendLayout();
       // 
       // listBox1
@@ -48,36 +59,117 @@
       this.label1.TabIndex = 1;
       this.label1.Text = "label1";
       // 
-      // label2
+      // pictureBox1
       // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(350, 20);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(35, 12);
-      this.label2.TabIndex = 2;
-      this.label2.Text = "label2";
+      this.pictureBox1.Location = new System.Drawing.Point(220, 85);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(50, 31);
+      this.pictureBox1.TabIndex = 14;
+      this.pictureBox1.TabStop = false;
       // 
-      // label3
+      // pictureBox2
       // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(500, 20);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(35, 12);
-      this.label3.TabIndex = 3;
-      this.label3.Text = "label3";
+      this.pictureBox2.Location = new System.Drawing.Point(370, 85);
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.Size = new System.Drawing.Size(50, 31);
+      this.pictureBox2.TabIndex = 15;
+      this.pictureBox2.TabStop = false;
+      // 
+      // pictureBox3
+      // 
+      this.pictureBox3.Location = new System.Drawing.Point(520, 85);
+      this.pictureBox3.Name = "pictureBox3";
+      this.pictureBox3.Size = new System.Drawing.Size(50, 31);
+      this.pictureBox3.TabIndex = 16;
+      this.pictureBox3.TabStop = false;
+      // 
+      // Temperature
+      // 
+      this.Temperature.AutoSize = true;
+      this.Temperature.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.Temperature.Location = new System.Drawing.Point(200, 125);
+      this.Temperature.Name = "Temperature";
+      this.Temperature.Size = new System.Drawing.Size(51, 24);
+      this.Temperature.TabIndex = 18;
+      this.Temperature.Text = "tempMax\r\ntempMin";
+      // 
+      // Date
+      // 
+      this.Date.AutoSize = true;
+      this.Date.Location = new System.Drawing.Point(200, 45);
+      this.Date.Name = "Date";
+      this.Date.Size = new System.Drawing.Size(54, 36);
+      this.Date.TabIndex = 19;
+      this.Date.Text = "dateLabel\r\n\r\ndate";
+      // 
+      // Situation
+      // 
+      this.Situation.AutoSize = true;
+      this.Situation.Location = new System.Drawing.Point(200, 169);
+      this.Situation.Name = "Situation";
+      this.Situation.Size = new System.Drawing.Size(50, 12);
+      this.Situation.TabIndex = 20;
+      this.Situation.Text = "Situation";
+      // 
+      // Date1
+      // 
+      this.Date1.AutoSize = true;
+      this.Date1.Location = new System.Drawing.Point(350, 46);
+      this.Date1.Name = "Date1";
+      this.Date1.Size = new System.Drawing.Size(60, 36);
+      this.Date1.TabIndex = 21;
+      this.Date1.Text = "dateLabel1\r\n\r\ndate1";
+      // 
+      // Date2
+      // 
+      this.Date2.AutoSize = true;
+      this.Date2.Location = new System.Drawing.Point(500, 45);
+      this.Date2.Name = "Date2";
+      this.Date2.Size = new System.Drawing.Size(60, 36);
+      this.Date2.TabIndex = 22;
+      this.Date2.Text = "dateLabel2\r\n\r\ndate2";
+      // 
+      // Temperature1
+      // 
+      this.Temperature1.AutoSize = true;
+      this.Temperature1.Location = new System.Drawing.Point(350, 125);
+      this.Temperature1.Name = "Temperature1";
+      this.Temperature1.Size = new System.Drawing.Size(57, 24);
+      this.Temperature1.TabIndex = 23;
+      this.Temperature1.Text = "tempMax1\r\ntempMin1\r\n";
+      // 
+      // Temperature2
+      // 
+      this.Temperature2.AutoSize = true;
+      this.Temperature2.Location = new System.Drawing.Point(500, 125);
+      this.Temperature2.Name = "Temperature2";
+      this.Temperature2.Size = new System.Drawing.Size(57, 24);
+      this.Temperature2.TabIndex = 24;
+      this.Temperature2.Text = "tempMax2\r\ntempMin2";
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(674, 372);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.label2);
+      this.Controls.Add(this.Temperature2);
+      this.Controls.Add(this.Temperature1);
+      this.Controls.Add(this.Date2);
+      this.Controls.Add(this.Date1);
+      this.Controls.Add(this.Situation);
+      this.Controls.Add(this.Date);
+      this.Controls.Add(this.Temperature);
+      this.Controls.Add(this.pictureBox3);
+      this.Controls.Add(this.pictureBox2);
+      this.Controls.Add(this.pictureBox1);
       this.Controls.Add(this.label1);
       this.Controls.Add(this.listBox1);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -87,8 +179,16 @@
 
     private System.Windows.Forms.ListBox listBox1;
     private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Label label2;
-    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.PictureBox pictureBox2;
+    private System.Windows.Forms.PictureBox pictureBox3;
+    private System.Windows.Forms.Label Temperature;
+    private System.Windows.Forms.Label Date;
+    private System.Windows.Forms.Label Situation;
+    private System.Windows.Forms.Label Date1;
+    private System.Windows.Forms.Label Date2;
+    private System.Windows.Forms.Label Temperature1;
+    private System.Windows.Forms.Label Temperature2;
   }
 }
 
--- WeatherCheckApp/Form1.cs
+++ WeatherCheckApp/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Codeplex.Data;
 using System.Net;
+using System.Globalization;
+
 
 
 namespace WeatherCheckApp {
@@ -18,94 +20,180 @@ namespace WeatherCheckApp {
     }
 
     private void Form1_Load(object sender, EventArgs e) {
-      Text = "WeatherCheckApp";
+      //タイトルバーにプログラム名を表示
+      this.Text = "WeatherCheckApp";
 
-        Dictionary<string, string> dictionary = new Dictionary<string, string>();
-        dictionary.Add("016010", "北海道(札幌)");
-        dictionary.Add("020010", "青森県");
-        dictionary.Add("030010", "岩手県(盛岡)");
-        dictionary.Add("040010", "宮城県(仙台)");
-        dictionary.Add("050010", "秋田県");
-        dictionary.Add("060010", "山形県");
-        dictionary.Add("070010", "福島県");
-        dictionary.Add("080010", "茨城県(水戸)");
-        dictionary.Add("090010", "栃木県(宇都宮)");
-        dictionary.Add("100010", "群馬県(前橋)");
-        dictionary.Add("110010", "埼玉県(さいたま)");
-        dictionary.Add("120010", "千葉県");
-        dictionary.Add("130010", "東京都");
-        dictionary.Add("140010", "神奈川県(横浜)");
-        dictionary.Add("150010", "新潟県");
-        dictionary.Add("160010", "富山県");
-        dictionary.Add("170010", "石川県(金沢)");
-        dictionary.Add("180010", "福井県");
-        dictionary.Add("190010", "山梨県(甲府)");
-        dictionary.Add("200010", "長野県");
-        dictionary.Add("210010", "岐阜県");
-        dictionary.Add("220010", "静岡県");
-        dictionary.Add("230010", "愛知県(名古屋)");
-        dictionary.Add("240010", "三重県(津)");
-        dictionary.Add("250010", "滋賀県(大津)");
-        dictionary.Add("260010", "京都府");
-        dictionary.Add("270000", "大阪府");
-        dictionary.Add("280010", "兵庫県(神戸)");
-        dictionary.Add("290010", "奈良県");
-        dictionary.Add("300010", "和歌山県");
-        dictionary.Add("310010", "鳥取県");
-        dictionary.Add("320010", "島根県(松江)");
-        dictionary.Add("330010", "岡山県");
-        dictionary.Add("340010", "広島県");
-        dictionary.Add("350020", "山口県");
-        dictionary.Add("360010", "徳島県");
-        dictionary.Add("370000", "香川県(高松)");
-        dictionary.Add("380010", "愛媛県(松山)");
-        dictionary.Add("390010", "高知県");
-        dictionary.Add("400010", "福岡県");
-        dictionary.Add("410010", "佐賀県");
-        dictionary.Add("420010", "長崎県");
-        dictionary.Add("430010", "熊本県");
-        dictionary.Add("440010", "大分県");
-        dictionary.Add("450010", "宮崎県");
-        dictionary.Add("460010", "鹿児島県");
-        dictionary.Add("471010", "沖縄県(那覇)");
-
-        listBox1.DataSource = new BindingSource(dictionary, null);
-        listBox1.DisplayMember = "Value";
-        listBox1.ValueMember = "Key";
-
+      //Dictionaryクラスの宣言(Key=string型、Value=string型)
+      Dictionary<string, string> prefecturalCapital = new Dictionary<string, string>();
+      //Add()メソッドでの(Key, Value)の追加
+        prefecturalCapital.Add("016010", "北海道(札幌)");
+        prefecturalCapital.Add("020010", "青森県");
+        prefecturalCapital.Add("030010", "岩手県(盛岡)");
+        prefecturalCapital.Add("040010", "宮城県(仙台)");
+        prefecturalCapital.Add("050010", "秋田県");
+        prefecturalCapital.Add("060010", "山形県");
+        prefecturalCapital.Add("070010", "福島県");
+        prefecturalCapital.Add("080010", "茨城県(水戸)");
+        prefecturalCapital.Add("090010", "栃木県(宇都宮)");
+        prefecturalCapital.Add("100010", "群馬県(前橋)");
+        prefecturalCapital.Add("110010", "埼玉県(さいたま)");
+        prefecturalCapital.Add("120010", "千葉県");
+        prefecturalCapital.Add("130010", "東京都");
+        prefecturalCapital.Add("140010", "神奈川県(横浜)");
+        prefecturalCapital.Add("150010", "新潟県");
+        prefecturalCapital.Add("160010", "富山県");
+        prefecturalCapital.Add("170010", "石川県(金沢)");
+        prefecturalCapital.Add("180010", "福井県");
+        prefecturalCapital.Add("190010", "山梨県(甲府)");
+        prefecturalCapital.Add("200010", "長野県");
+        prefecturalCapital.Add("210010", "岐阜県");
+        prefecturalCapital.Add("220010", "静岡県");
+        prefecturalCapital.Add("230010", "愛知県(名古屋)");
+        prefecturalCapital.Add("240010", "三重県(津)");
+        prefecturalCapital.Add("250010", "滋賀県(大津)");
+        prefecturalCapital.Add("260010", "京都府");
+        prefecturalCapital.Add("270000", "大阪府");
+        prefecturalCapital.Add("280010", "兵庫県(神戸)");
+        prefecturalCapital.Add("290010", "奈良県");
+        prefecturalCapital.Add("300010", "和歌山県");
+        prefecturalCapital.Add("310010", "鳥取県");
+        prefecturalCapital.Add("320010", "島根県(松江)");
+        prefecturalCapital.Add("330010", "岡山県");
+        prefecturalCapital.Add("340010", "広島県");
+        prefecturalCapital.Add("350020", "山口県");
+        prefecturalCapital.Add("360010", "徳島県");
+        prefecturalCapital.Add("370000", "香川県(高松)");
+        prefecturalCapital.Add("380010", "愛媛県(松山)");
+        prefecturalCapital.Add("390010", "高知県");
+        prefecturalCapital.Add("400010", "福岡県");
+        prefecturalCapital.Add("410010", "佐賀県");
+        prefecturalCapital.Add("420010", "長崎県");
+        prefecturalCapital.Add("430010", "熊本県");
+        prefecturalCapital.Add("440010", "大分県");
+        prefecturalCapital.Add("450010", "宮崎県");
+        prefecturalCapital.Add("460010", "鹿児島県");
+        prefecturalCapital.Add("471010", "沖縄県(那覇)");
+      //listBox1にprefecturalCapitalを表示
+      this.listBox1.DataSource = new BindingSource(prefecturalCapital, null);
+      this.listBox1.DisplayMember = "Value";
+      this.listBox1.ValueMember = "Key";
     }
 
     private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
       ListBox listbox = (ListBox)sender;
+      //DisplayMember：表示に使う値
       listbox.DisplayMember = "Value";
+      //ValueMember：実際の値
       listbox.ValueMember = "Key";
-      var url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + listbox.SelectedValue;
-      var req = WebRequest.Create(url);
 
-      using (var res = req.GetResponse())
-      using (var s = res.GetResponseStream()) {
-        dynamic json = DynamicJson.Parse(s);
+      //Livedoor Weather Web Service の基本URL + 地域のID
+      string url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + listbox.SelectedValue;
+      //リクエストを作成
+      WebRequest request = WebRequest.Create(url);
+      //レスポンスを取得
+      using (WebResponse response = request.GetResponse())
+      //ストリームを取得、{}内で()内が有効
+      using (System.IO.Stream stream = response.GetResponseStream()) {
+        //streamをdynamic型に変換
+        dynamic json = DynamicJson.Parse(stream);
+        
+        //今日の情報
+          dynamic today = json.forecasts[0];
+          string dateLabel = today.dateLabel;
+          string date = today.date;
+        //明日の情報
+          dynamic tomorrow = json.forecasts[1];
+          string dateLabel1 = tomorrow.dateLabel;
+          string date1 = tomorrow.date;
+        //明後日の情報
+          dynamic dayAfterTomorrow = json.forecasts[2];
+          string dateLabel2 = dayAfterTomorrow.dateLabel;
+          string date2 = dayAfterTomorrow.date;
 
-        dynamic today = json.forecasts[0];
-        string dateLabel = today.dateLabel;
-        string date = today.date;
-        string telop = today.telop;
-        var situation = json.description.text;
+        //天気概況
+          string situation = json.description.text;
+        //dateをDateTime型に変換
+          DateTime dt = DateTime.Parse(date);
+          DateTime dt1 = DateTime.Parse(date1);
+          DateTime dt2 = DateTime.Parse(date2);
+        //和暦でDateTime型を文字列に変換
+          CultureInfo japaneseCalendar = new CultureInfo("ja-JP", false);
+          japaneseCalendar.DateTimeFormat.Calendar = new JapaneseCalendar();
 
-        dynamic tomorrow = json.forecasts[1];
-        string dateLabel1 = tomorrow.dateLabel;
-        string date1 = tomorrow.date;
-        string telop1 = tomorrow.telop;
+        //今日の気温(Max & Min)
+          StringBuilder tempMax = new StringBuilder();
+          dynamic todayTempMax = today.temperature.max;
+          if (todayTempMax != null) {
+            tempMax.AppendFormat("{0}℃", todayTempMax.celsius);
+          }
+          else {
+            tempMax.Append("---");
+          }
+          StringBuilder tempMin = new StringBuilder();
+          dynamic todayTempMin = today.temperature.min;
+          if (todayTempMin != null) {
+            tempMin.AppendFormat("{0}℃", todayTempMin.celsius);
+          }
+          else {
+            tempMin.Append("---");
+          }
+        //明日の気温(Max & Min)
+          StringBuilder tempMax1 = new StringBuilder();
+          dynamic tomorrowTempMax = tomorrow.temperature.max;
+          if (tomorrowTempMax != null) {
+            tempMax1.AppendFormat("{0}℃", tomorrowTempMax.celsius);
+          }
+          else {
+            tempMax1.Append("---");
+          }
+          StringBuilder tempMin1 = new StringBuilder();
+          dynamic tomorrowTempMin = tomorrow.temperature.min;
+          if (tomorrowTempMin != null) {
+            tempMin1.AppendFormat("{0}℃", tomorrowTempMin.celsius);
+          }
+          else {
+            tempMin1.Append("---");
+          }
+        //明後日の気温(Max & Min)
+          StringBuilder tempMax2 = new StringBuilder();
+          dynamic dayafterTempMax = dayAfterTomorrow.temperature.max;
+          if (dayafterTempMax != null) {
+            tempMax2.AppendFormat("{0}℃", dayafterTempMax.celsius);
+          }
+          else {
+          tempMax2.Append("---");
+          }
+          StringBuilder tempMin2 = new StringBuilder();
+          dynamic dayafterTempMin = dayAfterTomorrow.temperature.min;
+          if (dayafterTempMin != null) {
+          tempMin2.AppendFormat("{0}℃", dayafterTempMin.celsius);
+          }
+          else {
+            tempMin2.Append("---");
+          }
 
-        dynamic dayaftertomorrow = json.forecasts[2];
-        string dateLabel2 = dayaftertomorrow.dateLabel;
-        string date2 = dayaftertomorrow.date;
-        string telop2 = dayaftertomorrow.telop;
+          this.label1.Text = listBox1.Text;
+          this.Date.Text = "<" + dateLabel + ">\n\n" + dt.ToString("gy年MM月dd日(ddd)", japaneseCalendar);
+          this.Date1.Text = "<" + dateLabel1 + ">\n\n" + dt1.ToString("gy年MM月dd日(ddd)", japaneseCalendar);
+          this.Date2.Text = "<" + dateLabel2 + ">\n\n" + dt2.ToString("gy年MM月dd日(ddd)", japaneseCalendar);
+      
+          this.Temperature.Text = "最高気温 : " + tempMax.ToString() + "\n最低気温 : " + tempMin.ToString();
+          this.Temperature1.Text = "最高気温 : " + tempMax1.ToString() + "\n最低気温 : " + tempMin1.ToString();
+          this.Temperature2.Text = "最高気温 : " + tempMax2.ToString() + "\n最低気温 : " + tempMin2.ToString();
+          
+          this.Situation.Text = situation;
 
-        label1.Text = listBox1.Text + "\n\n<" + dateLabel + ">" + "\n" + date
-          + "\n天気 : " + telop + "\n\n\n" + situation;
-        label2.Text = "\n\n<" + dateLabel1 + ">" + "\n" + date1 + "\n天気 : " + telop1;
-        label3.Text = "\n\n<" + dateLabel2 + ">" + "\n" + date2 + "\n天気 : " + telop2;
+        //天気画像
+          this.pictureBox1.ImageLocation = json.forecasts[0].image.url;
+          this.pictureBox2.ImageLocation = json.forecasts[1].image.url;
+          this.pictureBox3.ImageLocation = json.forecasts[2].image.url;
+
+        
+          
+         
+
+         
+          
       }
     }
   }
